0515a0830dd1ff5ac3798bf6e891c0b3b39bd523
Gabriel Nunes Barbosa Nogueira <72049404+Gabriel-boop-deep@users.noreply.github.com>
2023-07-16 22:19:29 +0000

    Criando pasta de resoluçao de exercicios do HackerRank

---
+++ Exercicios_HackerRank/purificacaoV2.js
@@ -0,0 +1,12 @@
+//let caracteresCorrompidos = [ '*', '%' , '!', '@' , '$','&' ,'(' , ')'];
+
+let texto = "Felis) silvestris *catus&",merge='';
+
+let dadosCorrompidos = '*%!@$&()';
+//Se ele não encontrar nenhum caractere especial vai retornar -1 então ele pode escrever
+for (let i=0;i<texto.length;i++){
+   if(dadosCorrompidos.indexOf(texto[i])==-1){
+    merge+=texto[i];
+   }
+}
+console.log(merge);
